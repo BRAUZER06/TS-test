fb7358722ddde3f575103c9f498e81b778549cd5
AHMATOLOG <murtaz.oziev@mail.ru>
2022-03-04 20:54:50 +0300

    pust

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { title } from "process";
+import React from "react";
 
-function App() {
+type TitleProps = {
+  title: string;
+  children?:string
+  test?:string
+};
+
+const Title = ({ title, test = 'test' }: TitleProps) => <h1>{title}{test}</h1>;
+
+const SectionLeft = ({ title, children }:TitleProps) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>
+      {title}
+      {children}
+    </h1>
   );
-}
+};
+
+const App = () => <Title title='test'/>
 
 export default App;
